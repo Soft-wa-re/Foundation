a7a29ccf472aff8ee2a6e9caa051c953b2c13dbb
Tom Beckenhauer <tbeckenhauer@users.noreply.github.com>
2021-09-24 22:14:40 -0500

    Update runBlogCast.sh

--- runBlogCast.sh
+++ runBlogCast.sh
@@ -2,4 +2,4 @@
 
 docker pull tmbecken/blog_to_podcast
 
-docker run --rm -it -v "$(pwd):/home/testuser/workspace/InputProject" tmbecken/blog_to_podcast ../BlogCast/main.py
+docker run --rm -v "$(pwd):/home/testuser/workspace/InputProject" tmbecken/blog_to_podcast ../BlogCast/main.py
